2bac1aa6134bad91e58dc9a0c4705c509ef1deee
tarcisio <sassara.tarcisio@gmail.com>
2018-01-22 23:01:37 -0200

    imagens

--- README.md
+++ README.md
@@ -8,11 +8,14 @@ Site miaversa.
   - nu html5 validator versão 17.11.1
   - AWS cli - aws-cli/1.14.19 Python/3.5.2 Linux/4.10.0-42-generic botocore/1.8.23
   - crass css compressor
+  - jpegtran
 
 No diretório `bin` encontra-se os executáveis.
 
 O comando `smake` gera o site e espera uma variável de ambiente chamada `SALT`.
 O `SALT` serve para assinar as chamadas para o carrinho de compras de forma segura.
+A variável de ambiente `DEBUG` pode ser utilizada no ambiente de desenvolvimento para reduzir o tempo
+de processamento, eliminando a etapa de compressão do CSS.
 
 ### Exemplo de chamada do comando
 
@@ -22,6 +25,10 @@ O comando `html5validator` é um atalho para chamar o validador nu.
 
 O comando `update` é um atalho para atualizar o bucket S3.
 
-para o estilo css é preciso instalar o nodejs e depois o crass:
+Para o estilo css é preciso instalar o nodejs e depois o crass:
 
     npm install --save-dev crass
+
+Para reduzir o tamanho das imagens é preciso instalar o jpegtran:
+
+    sudo apt install libjpeg-progs
---
+++ bin/resize-images.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# $1 caminho da imagem
+# $2 numero de iteracoes
+# $3 tamanho da imagem padrao
+function resize_media_forty_three {
+	def=$3
+	root=`dirname $1`
+	filename=`basename $1`
+	extension=".${filename##*.}"
+	filename="${filename%.*}"
+	filename_reduzido="${filename%-*}"
+
+	for n in `seq 1 $2`;
+	do
+		cent="$n"00
+		original=$1
+		interdir="$root/tmp/"
+		interfile="$root/tmp/$filename$extension"
+		destino="$root/$filename_reduzido-$cent$extension"
+		mkdir -p $interdir
+		mogrify -path $interdir -filter Triangle -define filter:support=2 -thumbnail $cent -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip $original
+		jpegtran -copy none -optimize $interfile > $destino
+	done
+
+	original=$1
+	interdir="$root/tmp/"
+	interfile="$root/tmp/$filename$extension"
+	destino="$root/$filename_reduzido$extension"
+	mkdir -p $interdir
+	mogrify -path $interdir -filter Triangle -define filter:support=2 -thumbnail $def -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip $original
+	jpegtran -copy none -optimize $interfile > $destino
+
+	rm -rf $interdir
+}
+
+resize_media_forty_three "content/images/media/serrando-original.jpg" 9 400
+resize_media_forty_three "content/images/media/recozendo-original.jpg" 9 400
