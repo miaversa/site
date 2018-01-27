5f9215567e6694478ffc62e42996ffbfd5f990ae
tarcisio <sassara.tarcisio@gmail.com>
2018-01-27 02:16:54 -0200

    env

--- carrinho/bootstrap.php
+++ carrinho/bootstrap.php
@@ -4,17 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/functions.php';
 require __DIR__ . '/cart.php';
 
-define('DEBUG', 1);
-
-$salt = getenv('SALT');
-if($salt == false) {
-	echo "salt not set";
-	exit();
+if (file_exists(__DIR__ . '/env.pro.php')) {
+	require __DIR__ . '/env.pro.php';
+} else {
+	require __DIR__ . '/env.dev.php';
 }
-
-define('SALT', $salt);
-
-$site = [
-	'siteurl' => 'http://miaversa.com.br:8080',
-	'analytics' => 'UA-112427178-1'
-];
---
+++ carrinho/env.dev.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DEBUG', 1);
+define('SALT', 'x');
+
+$site = [
+	'siteurl' => 'http://miaversa.com.br:8080',
+	'analytics' => 'UA-112427178-1'
+];
