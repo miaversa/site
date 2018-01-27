0ad6b1f2e348d1f5a2a8e643952a5033dce65109
tarcisio <sassara.tarcisio@gmail.com>
2018-01-27 20:05:22 -0200

    cart

--- carrinho/public/shipping.php
+++ carrinho/public/shipping.php
@@ -19,28 +19,28 @@ if('POST' == $_SERVER['REQUEST_METHOD']) {
 		'postalCode' => '',
 	]];
 
-	if (isset($_POST['data']['address']['street']) {
+	if (isset($_POST['data']['address']['street'])) {
 		$data['address']['street'] = $_POST['data']['address']['street'];
 	}
-	if (isset($_POST['data']['address']['number']) {
+	if (isset($_POST['data']['address']['number'])) {
 		$data['address']['number'] = $_POST['data']['address']['number'];
 	}
-	if (isset($_POST['data']['address']['complement']) {
+	if (isset($_POST['data']['address']['complement'])) {
 		$data['address']['complement'] = $_POST['data']['address']['complement'];
 	}
-	if (isset($_POST['data']['address']['district']) {
+	if (isset($_POST['data']['address']['district'])) {
 		$data['address']['district'] = $_POST['data']['address']['district'];
 	}
-	if (isset($_POST['data']['address']['city']) {
+	if (isset($_POST['data']['address']['city'])) {
 		$data['address']['city'] = $_POST['data']['address']['city'];
 	}
-	if (isset($_POST['data']['address']['state']) {
+	if (isset($_POST['data']['address']['state'])) {
 		$data['address']['state'] = $_POST['data']['address']['state'];
 	}
-	if (isset($_POST['data']['address']['country']) {
+	if (isset($_POST['data']['address']['country'])) {
 		$data['address']['country'] = $_POST['data']['address']['country'];
 	}
-	if (isset($_POST['data']['address']['postalCode']) {
+	if (isset($_POST['data']['address']['postalCode'])) {
 		$data['address']['postalCode'] = $_POST['data']['address']['postalCode'];
 	}
 
