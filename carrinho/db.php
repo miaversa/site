e9c5ac94ac04f8ced9bea2f7038dd6a3215f09f7
tarcisio <sassara.tarcisio@gmail.com>
2018-01-27 18:21:48 -0200

    cart

--- carrinho/db.php
+++ carrinho/db.php
@@ -2,7 +2,15 @@
 
 function auth($email, $password)
 {
-	$dynamo = new \Aws\DynamoDb\DynamoDbClient(['region' => 'sa-east-1', 'version' => 'latest']);
+	$dynamo = new \Aws\DynamoDb\DynamoDbClient([
+		'region' => 'sa-east-1',
+		'version' => 'latest',
+		'credentials' => [
+			'key'    => AWS_KEY,
+			'secret' => AWS_SECRET,
+		],
+	]);
+	
 	$u = $dynamo->getItem([
 		'Key' => [
 			'email' => [
--- carrinho/env.dev.php
+++ carrinho/env.dev.php
@@ -3,6 +3,9 @@
 define('DEBUG', false);
 define('SALT', 'x');
 
+define('AWS_KEY', 'x');
+define('AWS_SECRET', 'x');
+
 $site = [
 	'siteurl' => 'http://miaversa.com.br:8080',
 	'analytics' => 'UA-112427178-1'
